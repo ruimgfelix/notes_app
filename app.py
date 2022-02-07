82141f84822ca5bbffb4d86ef5d8b3b5f7b26dc5
Rui Félix <gaimotafelix@gmail.com>
2022-02-07 22:58:24 +0000

    Added template inheritance; added logic for app.py methods and restricted the methods to only use GET or POSTS.

--- app.py
+++ app.py
@@ -3,14 +3,47 @@ import datetime
 app = Flask(__name__)
 
 my_notes = []
+initial_note = {}
+views = {
+    "list": 0,
+    "create_edit": 1
+}
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/menu')
+@app.route('/menu', methods=['GET'])
 def menu():
-    return render_template('menu.html', my_notes = my_notes)
+    return render_template('content.html', my_notes = my_notes, new_note = {}, view=views["list"])
+
+@app.route('/create', methods=['GET'])
+def create():
+    return render_template('content.html', my_notes = my_notes, new_note = {}, view=views["create_edit"])
+
+@app.route('/edit_note/<int:note_id>', methods=['GET'])
+def edit_note(note_id):
+    edit_note = {}
+    if len(my_notes) > 0:
+        edit_note = getNote(note_id)
+    else:
+        return redirect(url_for('menu'))
+    return render_template('content.html', my_notes = my_notes, new_note = edit_note, view=views["create_edit"])
+
+
+@app.route('/submit_edit_note/<int:note_id>', methods=['POST'])
+def submit_edit_note(note_id):
+    prev_note = getNote(note_id)
+    if request.method == 'POST':
+        note_title = request.form.get('inputNoteTitle')
+        note_description = request.form.get('inputNoteDescription')
+        my_notes[note_id] = {
+            "note_id": note_id,
+            "note_title": note_title,
+            "note_description": note_description,
+            "note_created_date": prev_note["note_created_date"]
+        }
+        return render_template('content.html', my_notes = my_notes, new_note = {}, view=views["list"])
 
 @app.route('/submit_note', methods=['POST'])
 def submit_note():
@@ -19,12 +52,21 @@ def submit_note():
         note_title = request.form.get('inputNoteTitle')
         note_description = request.form.get('inputNoteDescription')
         new_note = {
+            "note_id": getNextNoteId(),
             "note_title": note_title,
             "note_description": note_description,
             "note_created_date": today.strftime("%d %B %Y, %H:%M:%S")
         }
         addNotes(new_note)
         return redirect(url_for('menu'))
+
+@app.route('/remove_note/<int:note_id>', methods=['GET'])
+def remove_note(note_id):
+    if len(my_notes) > 0: 
+        my_notes.pop(note_id)
+    return redirect(url_for('menu'))
+
+
 
 def loadNotes(my_notes):
     my_notes = []
@@ -33,6 +75,12 @@ def loadNotes(my_notes):
 def addNotes(note):
     my_notes.append(note)
 
+def getNextNoteId():
+    return len(my_notes)
+
+def getNote(note_id):
+    note = my_notes[note_id]
+    return note
 
 if __name__ == '__main__':
     app.debug = True
--- static/start.js
+++ static/start.js
@@ -1,16 +1,37 @@
 $(document).ready(function(){
-    $(".list-notes").show();
-    $(".notes-form").hide();
+    /*$(".list_container").show();
+    $(".create_edit_container").hide();
 
     $(".toggle-show-notes").click(function(){
-      $(".notes-form").hide();
-      $(".list-notes").show();
+      $(".create_edit_container").hide();
+      $(".list_container").show();
     });
 
     $(".toggle-create-notes").click(function(){
-      $(".notes-form").show();
-      $(".list-notes").hide();
+      $(".create_edit_container").show();
+      $(".list_container").hide();
     });
+
+    $("#editNote").click(function() {
+      $(".create_edit_container").show();
+      $(".list_container").hide();
+    });*/
+    /* $("#editNote").click(function() {
+      alert("aaa");
+      window.location = 
+      $.ajax({
+        type: 'GET',
+        url: "/edit_note/" + $(this).attr("note_value"),
+        success: function(data){ window.location =  }
+      });
+      //console.log($(this).attr("note_value")); */
+    })
+    /*$("#editNote").ajax("http://www.example.com/", {
+      type: 'POST',
+      data: {
+        test: 'test1',
+        test2: 'test2'
+    }*/
 
 
 });
